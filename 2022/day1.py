5e8b0938e0fc35a5edb01423adf8690e5035b325
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2022-12-02 09:24:21 +0100

    all in one

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,24 +1,8 @@
 # Find the Elf carrying the most Calories. 
 # How many total Calories is that Elf carrying?
 
-def part_one(path):
-    '''Find the Elf carrying the most Calories and his total Calories'''
-    max = 0
-    current_sum = 0
-
-    with open(path, 'r') as file:
-        for line in file.readlines():
-            if line == "\n":
-                current_sum = 0
-                continue
-
-            current_sum += int(line)
-            if max < current_sum:
-                max = current_sum    
-    return max
-
-def part_two(path):
-    '''Find top 3 elves with most calories and their total calories'''
+def day_1_full(path):
+    '''Find the top 3 calories carried by the elves and return both top 1 and top 3'''
     calories = []
     current_cal = 0
 
@@ -34,17 +18,15 @@ def part_two(path):
             # End Of File
              calories.append(current_cal)
 
-    return sum(sorted(calories, reverse=True)[:3])
-
+    calories = sorted(calories, reverse=True)[:3]
+    return (calories[0], sum(calories))
 
 def main():
     input = "./2022/inputs/day1/day1.txt"
     
-    max = part_one(input)
-    print("Part 1 - output: "+str(max))
-
-    total = part_two(input)
-    print("Part 2 - output: "+str(total))
+    (one, two) = day_1_full(input)
+    print("Part 1: "+ str(one))
+    print("Part 2: "+str(two))
 
 if __name__ == '__main__':
     main()
