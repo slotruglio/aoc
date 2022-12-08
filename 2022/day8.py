3adc8ea591592440ecd102969f62ad697e26d8ca
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2022-12-08 07:10:48 +0100

    add part 2

--- 2022/day8.py
+++ 2022/day8.py
@@ -7,6 +7,7 @@ def day_eight(input):
             matrix.append([ int(x) for x in line.strip() ])
 
     visibles = 2*(len(matrix)-1) + 2*(len(matrix[0])-1)
+    scenic_score = 0
     for i in range(1, len(matrix) -1):
         for j in range(1, len(matrix[i]) - 1):
             # row left
@@ -25,8 +26,36 @@ def day_eight(input):
             if matrix[i][j] > max(e[j] for e in matrix[i+1:]):
                 visibles += 1
                 continue
+            
+        # count number of visible cells
+        for j in range(1, len(matrix[i]) - 1):
+            left = 0
+            # row left
+            for k in range(j-1, -1, -1):
+                left += 1
+                if matrix[i][k] >= matrix[i][j]:
+                    break
+            # row right
+            right = 0
+            for k in range(j+1, len(matrix[i])):
+                right += 1
+                if matrix[i][k] >= matrix[i][j]:
+                    break
+            # col up
+            up = 0
+            for k in range(i-1, -1, -1):
+                up += 1
+                if matrix[k][j] >= matrix[i][j]:
+                    break
+            # col down
+            down = 0
+            for k in range(i+1, len(matrix)):
+                down += 1
+                if matrix[k][j] >= matrix[i][j]:
+                    break
+            scenic_score = max(scenic_score, left*right*up*down)
 
-    return (visibles,0)
+    return (visibles,scenic_score)
 
 
 if __name__ == '__main__':
