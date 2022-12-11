e728e1ae05dcd3c5aa7fe787d7e3d082ff1fa4d1
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2022-12-11 17:30:19 +0100

    add part 2

--- 2022/day11.py
+++ 2022/day11.py
@@ -1,46 +1,60 @@
-import math
+import numpy as np
+import copy
+from tqdm import tqdm
+
+def get_top_2(monkeys, rounds, LCM=None): 
+    top_2 = [0,0]
+    for i in tqdm(range(rounds)):
+        for monkey in monkeys:
+            monkey.do_routine(monkeys, LCM)
+
+        if i == rounds - 1:
+            top_2 = sorted([m.inspected for m in monkeys], reverse=True)[:2]
+    return top_2
 
 class Monkey:
-    def __init__(self, num):
+    def __init__(self, name=None, num=None, items=None, operation=None, test_val=None, true_m=None, false_m=None):
         self.inspected = 0
-        self.name = "Monkey "+str(num)
-        self.items = []
-        self.operation = None
-        self.test_fn = None
-        self.true_m = None
-        self.false_m = None
+        self.name = name if name else "Monkey " + str(num)
+        self.items = items
+        self.operation = operation
+        self.test_val = test_val
+        self.true_m = true_m
+        self.false_m = false_m
     
+    def __deepcopy__(self, memo):
+        return Monkey(name = self.name, items = copy.deepcopy(self.items), operation = self.operation, test_val = self.test_val, true_m = self.true_m, false_m = self.false_m)
+
     def parse_op(operation):
         _, op = operation.split("=")
         def fn(old):            
             return eval(op)
         return fn
 
-    def parse_test(test):
-        num = test.split(" ")[-1]
-        def fn(value):
-            return value % int(num) == 0
-        return fn
-
     def add(self, item):
-        self.items.append(item)
+        self.items = np.append(self.items, item)
 
-    def inspect(self, division):
-        self.items[0] = self.operation(self.items[0])
-        if division:
-            self.items[0] = math.floor(self.items[0] / 3)
-        self.inspected += 1
+    def inspect(self, LCM=None):
+        self.items = self.operation(self.items)
+        if LCM:
+            self.items = self.items % LCM
+        else:
+            self.items = self.items // 3
+
+        count = len(self.items)
+        self.inspected += count
+        return count
 
     def test(self, monkeys):
-        item = self.items.pop(0)
-        index = self.true_m if self.test_fn(item) else self.false_m
-        monkeys[index].add(item)
+        for item in self.items:
+            index = self.true_m if item % self.test_val == 0 else self.false_m
+            monkeys[index].add(item)
     
-    def do_routine(self, division, monkeys):
-        count = len(self.items)
-        for _ in range(count):
-            self.inspect(division)
-            self.test(monkeys)
+    def do_routine(self, monkeys, LCM=None):
+        count = self.inspect(LCM)
+        rounds = range(count)
+        self.test(monkeys)
+        self.items = np.delete(self.items, rounds)
 
 def day_eleven(input):
     monkeys = []
@@ -50,27 +64,26 @@ def day_eleven(input):
             if "Monkey" in line:
                 monkey = Monkey(len(monkeys))
             elif "items" in line:
-                monkey.items = [int(x) for x in line.split(":")[1].split(",")]
+                monkey.items = np.array([int(x) for x in line.split(":")[1].split(",")], dtype=np.uint64)
             elif "Operation" in line:
                 monkey.operation = Monkey.parse_op(line.split(":")[-1].strip())
             elif "Test" in line:
-                monkey.test_fn = Monkey.parse_test(line.split(":")[-1].strip())
+                monkey.test_val = int(line.split(":")[-1].strip().split(" ")[-1])
             elif "true" in line:
                 monkey.true_m = int(line.split(" ")[-1])
             elif "false" in line:
                 monkey.false_m = int(line.split(" ")[-1])
                 monkeys.append(monkey)
     
+    divisors = []
+    for monkey in monkeys:
+        divisors.append(monkey.test_val)
+    LCM = np.lcm.reduce(divisors)
+
     # run the monkeys
-    top_2 = [0,0]
-    for i in range(20):
-        for monkey in monkeys:
-            monkey.do_routine(division=True, monkeys=monkeys)
-
-        if i == 19:
-            top_2 = sorted([m.inspected for m in monkeys], reverse=True)[:2]
-
-    return (top_2[0]*top_2[1],0)
+    part_one = get_top_2(copy.deepcopy(monkeys), 20)
+    part_two = get_top_2(monkeys, 10_000, LCM)
+    return (part_one[0]*part_one[1], part_two[0]*part_two[1])
 
 if __name__ == '__main__':
     one,two = day_eleven("2022/inputs/day11/input.txt")
